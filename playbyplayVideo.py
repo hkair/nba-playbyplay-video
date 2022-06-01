fee1666d012ce5e5dbe7a634d1e0ba2f85c5190a
hobin <hb2kang@gmail.com>
2022-06-01 17:53:02 -0400

    Add game playbyplay video download

--- playbyplayVideo.py
+++ playbyplayVideo.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 from telnetlib import ECHO
 import requests
@@ -13,6 +14,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
+
+import time
+import datetime
 
 try:
     # For Python 3.0 and later
@@ -23,11 +28,8 @@ except ImportError:
 
 # Web Scraping Endpoint - /stats/events
 BASE_URL = "https://www.nba.com/stats/events?"
+# URL Example: "https://www.nba.com/stats/events/?CFID=&CFPARAMS=&ContextMeasure=FGA&EndPeriod=0&EndRange=28800&GameID=0042100307&RangeType=0&Season=2021-22&SeasonType=Playoffs&StartPeriod=0&StartRange=0&flag=3"
 
-# Required Parameters
-GameId = "0042100307"
-SeasonId = "2021-22"
-SeasonType="Playoffs"
 # flag - 1 (Video Only), flag - 2 (Plots Only), flag - 3 (Both)
 flag = 3
 
@@ -39,7 +41,6 @@ RangeType = 0
 StartPeriod = 0 
 StartRange = 0
 
-# URL Example: "https://www.nba.com/stats/events/?CFID=&CFPARAMS=&ContextMeasure=FGA&EndPeriod=0&EndRange=28800&GameID=0042100307&RangeType=0&Season=2021-22&SeasonType=Playoffs&StartPeriod=0&StartRange=0&flag=3"
 
 def extractPlayByPlayVideos(output_dir, GameId, SeasonId, SeasonType):
     '''
@@ -64,116 +65,98 @@ def extractPlayByPlayVideos(output_dir, GameId, SeasonId, SeasonType):
 
     # Selenium
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.implicitly_wait(10)
     driver.get(req.url)
-
 
     # Click on All in row selector of table
     # <select class="stats-table-pagination__select ng-pristine ng-valid ng-not-empty ng-touched >
     selector_div = driver.find_element(By.CLASS_NAME, "stats-table-pagination__info")
     select = Select(selector_div.find_element(By.TAG_NAME, "select"))
-    select.select_by_value('string:All')
+    options = select.options
 
-    print("Selector Clicked: All")
+    print(selector_div.text)
 
-    # Row Description
-    # <div class="nba-stat-table__overflow" data-fixed="1" role="grid">
-    stats_table_overflow = driver.find_element(By.CLASS_NAME, "nba-stat-table__overflow")
-    stats_table_overflow_tbody = stats_table_overflow.find_element(By.TAG_NAME, "tbody")
-    rows_overflow = stats_table_overflow_tbody.find_elements(By.TAG_NAME, "tr")
+    start = datetime.datetime.now()
 
-    # Button
-    # <div class="nba-stat-table__overlay" data-fixed="1" role="grid">
-    stats_table_overlay = driver.find_element(By.CLASS_NAME, "nba-stat-table__overlay")
-    stats_table_overlay_tbody = stats_table_overlay.find_element(By.TAG_NAME, "tbody")
-    rows_overlay = stats_table_overlay_tbody.find_elements(By.TAG_NAME, "tr")
-
-    print("nba-stat-table__overflow Row Length: ", len(rows_overflow))
-    print("nba-stat-table__overlay Row Length: ", len(rows_overlay))
-
-    n = len(rows_overflow)
-    
-    # overflow contain play by play information 
-    # overlay contains the button td
-    for idx, x in enumerate(zip(rows_overflow, rows_overlay)):
-        overflow, overlay = x
-
-        # List of td tags
-        overflow_td = overflow.find_elements(By.TAG_NAME, "td")
-        overlay_td = overlay.find_element(By.TAG_NAME, "td")
-
-        # # Click Button for next Video to Play
-        button_td = overflow_td[0].find_element(By.TAG_NAME, "button")
-        #button_td = overlay_td.find_element(By.TAG_NAME, "button")
-        print(button_td.get_attribute("outerHTML"))
-        #WebDriverWait(driver, 10)
-
-        button_td.click()
-
-        #overflow_td[1].click()
+    # iterate over selector 
+    # start with option 1, and skip the string:All
+    num_of_videos = 0
+    option_idx = 1
+    while option_idx < len(options):
         
-        #Extract Video
-        video = driver.find_element(By.ID, 'stats-videojs-player_html5_api')
-        video_url = video.get_attribute('src')
+        curr_option = options[option_idx].get_attribute("value")
+        select.select_by_value(curr_option)
+        print("Selector Clicked: ", curr_option)
 
-        # File Name
-        pbp_text = [ overflow_td[i].text for i in range(len(overflow_td)) ]
-        print(pbp_text)
-        description = [ "_".join(overflow_td[i].text.lower().split()) for i in range(1, 5) ]
-        file_name = "-".join(description)
-        print(file_name)
+        # Row Description
+        # <div class="nba-stat-table__overflow" data-fixed="1" role="grid">
+        stats_table_overflow = driver.find_element(By.CLASS_NAME, "nba-stat-table__overflow")
+        stats_table_overflow_tbody = stats_table_overflow.find_element(By.TAG_NAME, "tbody")
+        rows_overflow = stats_table_overflow_tbody.find_elements(By.TAG_NAME, "tr")
 
-        rsp = urlopen(video_url)
-        dest_file = os.path.join(output_dir, GameId, file_name + ".mp4")
-        print(dest_file)
-        with open(dest_file, 'wb') as f:
-            f.write(rsp.read())
+        print("Wait 1 seconds: ")
+        time.sleep(1)
 
-        if idx == 1:
-            break
+        # overflow contains play by play information 
+        # overlay contains the button td
+        prev_url = ''
+        skipped_videos = []
+        video_count = 0
+        for idx, overflow in enumerate(rows_overflow):
 
-    # i=0
-    # while i < n:
-    #     overflow, overlay = rows_overflow[i], rows_overlay[i]
+            # List of td tags
+            overflow_td = overflow.find_elements(By.TAG_NAME, "td")
 
-    #      # List of td tags
-    #     overflow_td = overflow.find_elements(By.TAG_NAME, "td")
-    #     overlay_td = overlay.find_element(By.TAG_NAME, "td")
-        
-    #     #Extract Video
-    #     video = driver.find_element(By.ID, 'stats-videojs-player_html5_api')
-    #     video_url = video.get_attribute('src')
+            # # Click Button for next Video to Play
+            button_td = overflow_td[0].find_element(By.TAG_NAME, "button")
+            driver.execute_script("arguments[0].click();", button_td)
+            print("Row Clicked: ")
 
-    #     # File Name
-    #     pbp_text = [ overflow_td[i].text for i in range(len(overflow_td)) ]
-    #     description = [ "_".join(overflow_td[i].text.lower().split()) for i in range(1, 5) ]
-    #     file_name = "-".join(description)
-    #     print(file_name)
+            video = driver.find_element(By.ID, 'stats-videojs-player_html5_api')
+            video_url = video.get_attribute('src')
 
-    #     rsp = urlopen(video_url)
-    #     dest_file = os.path.join(output_dir, GameId, file_name + ".mp4")
-    #     print(dest_file)
-    #     with open(dest_file, 'wb') as f:
-    #         f.write(rsp.read())
+            # If the new video_url is the same as prev url
+            # skip video
+            if video_url == prev_url:
+                prev_url = video_url
+                skipped_videos.append(video_url)
+                continue
 
-    #     # # Click Button for next Video to Play
-    #     overflow_next, overlay_next = rows_overflow[i+1], rows_overlay[i+1]
-    #     overflow_td = overflow_next.find_elements(By.TAG_NAME, "td")
-    #     overlay_td = overlay_next.find_element(By.TAG_NAME, "td")
+            print("Video URL: ", video_url)
 
-    #     # button_td = overflow_td[0].find_element(By.TAG_NAME, "button")
-    #     button_td = overlay_td.find_element(By.TAG_NAME, "button")
-    #     # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, "button")))
-    #     print(button_td.get_attribute("outerHTML"))
-    #     button_td.click()
+            # File Name
+            pbp_text = [ overflow_td[i].text for i in range(len(overflow_td)) ]
+            description = [ "_".join(pbp_text[i].lower().split()) for i in range(1, 5) ]
+            file_name =  GameId + "_{0}_".format(num_of_videos + idx) + "-".join(description) +  ".mp4"
 
-    #     if i == 1:
-    #         break
+            rsp = urlopen(video_url)
+            dest_file = os.path.join(output_dir, GameId, file_name)
+            print(dest_file)
+            with open(dest_file, 'wb') as f:
+                f.write(rsp.read())
 
-    #     i += 1
+            print("Downloaded Video: ", file_name)
 
+            video_count += 1
+
+        # add to the number of videos downloaded
+        num_of_videos += video_count
+        # move to next option in selector
+        option_idx += 1
+
+    end = datetime.datetime.now()
+    print("Elapsed Time: ", end-start)
+    print("{0} videos downloaded".format(num_of_videos))
+    print("Videos Skipped: ", skipped_videos)
 
 def main():
+    # Required Parameters
+    GameId = "0042100307"
+    SeasonId = "2021-22"
+    SeasonType="Playoffs"
+
     output_dir = "./data/videos/playbyplay-game-videos"
+
     extractPlayByPlayVideos(output_dir, GameId, SeasonId, SeasonType)
 
 
